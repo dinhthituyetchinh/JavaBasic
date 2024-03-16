b40f026a856c7c4210d7a0ca85abd7e4f1a818b1
dinhthituyetchinh <fengdinh@gmail.com>
2024-03-16 12:43:15 +0700

    updates 16

--- ExerciseSynthesis/Chapter1/Exercise1_23/src/exercise1_23/MangHaiChieu.java
+++ ExerciseSynthesis/Chapter1/Exercise1_23/src/exercise1_23/MangHaiChieu.java
@@ -24,7 +24,7 @@ public class MangHaiChieu
     {
         do
         {           
-            MENU();
+             MENU();
             System.out.format("\nMoi ban nhap lua chon: ");
             c = scanner.nextInt();
             switch (c)
@@ -41,9 +41,11 @@ public class MangHaiChieu
                     switch (x)
                     {
                         case 1 -> {
+                           nhapMaTran();
                             System.out.format("Tao mang thanh cong.\n");
                         }
                         case 2 -> {
+                            sinhMaTran();
                             System.out.format("Tao mang thanh cong.\n");
                         }
                         default -> System.out.format("Ban nhap sai, hay chon 1 hoac 2 de thuc hien chuong trinh.\nVui long nha Enter de tiep tuc.\n");
@@ -54,6 +56,8 @@ public class MangHaiChieu
             case 2 -> {
                 if (dong != 0 || cot != 0)
                 {
+                     System.out.format(" 2. Xuat mot ma tran chua n so nguyen.\n");
+                    xuatMaTran();
                 }
                 else
                 {
@@ -62,89 +66,99 @@ public class MangHaiChieu
                 }
             }
             case 3 -> {
-//                    if (dong != 0 || cot != 0)
-//                    {
-//                        System.out.format(" 3. Xuat cac phan tu o vi tri chan trong mang\n");
-//                        xuatMang(a, n);
-//                        System.out.format("\nCac phan tu o vi tri chan la:\n");
-//                        xuatCacPhanTuOViTriChan(a, n);
-//                        System.out.format("\n");
-//                    }
-//                    else
-//                    {
-//                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
-//                        System.out.format("Hay nhan Enter de tiep tuc.\n");
-//                    }
+                    if (dong != 0 || cot != 0)
+                    {
+                        System.out.format(" 3. Tính tổng giá trị các số lẻ.\n");
+                        xuatMaTran();
+                        System.out.println("Tổng giá trị các số lẻ: "+tinhTongCacGiaTriLe());
+                    } 
+                    else
+                    {
+                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
+                        System.out.format("Hay nhan Enter de tiep tuc.\n");
+                    }
             }
             case 4 -> {
-//                    if (dong != 0 || cot != 0)
-//                    {
-//                        System.out.format(" 4. Dem phan tu duong trong mang.\n");
-//                        xuatMang(a, n);
-//                        System.out.format("\nSo phan tu duong trong mang: %d\n", demPTDuong(a, n));
-//                    }
-//                    else
-//                    {
-//                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
-//                        System.out.format("Hay nhan Enter de tiep tuc.\n");
-//                    }
+                    if (dong != 0 || cot != 0)
+                    {
+                        System.out.format(" 4. Tính tổng các giá trị dương trên dòng k cho trước.\n");
+                        xuatMaTran();
+                        int k;
+                        System.out.println("Nhập dòng k: ");
+                        k = scanner.nextInt();
+                        System.out.println("Tổng các giá trị dương trên "+k+" la: "+tinhTongCacGiaTriDuongTrenK(k));
+                    }
+                    else
+                    {
+                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
+                        System.out.format("Hay nhan Enter de tiep tuc.\n");
+                    }
             }
             case 5 -> {
-//                    if (dong != 0 || cot != 0)
-//                    {
-//                        System.out.format(" 5. Tong cac phan tu am trong mang\n");
-//                        xuatMang(a, n);
-//                        System.out.format("\nTong cac phan tu am trong mang: %d\n", tongCacPTAm(a, n));
-//                    }
-//                    else
-//                    {
-//                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
-//                        System.out.format("Hay nhan Enter de tiep tuc.\n");
-//                    }
+                    if (dong != 0 || cot != 0)
+                    {
+                        System.out.format(" 5. Đếm số lượng số dương.\n");
+                        xuatMaTran();
+                        System.out.println("Số lượng các số dương trong ma trận: "+ demSoDuong());
+                    }
+                    else
+                    {
+                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
+                        System.out.format("Hay nhan Enter de tiep tuc.\n");
+                    }
             }
             case 6 -> {
-//                    if (dong != 0 || cot != 0)
-//                    {
-//                        System.out.format(" 6. Tim vi tri cua gia tri x trong mang.\n");
-//                        xuatMang(a, n);
-//                        int x;
-//                        System.out.format("\nNhap gia tri ma ban muon tim: ");
-//                        scanf_s("%d", &x);
-//                        if (viTriCuaGiaTriX(a, n, x) == -1)
-//                        {
-//                            System.out.format("Phan tu khong ton tai trong mang.\n");
-//                        }
-//                        else
-//                        {
-//                            System.out.format("\nVi tri cua gia tri x trong mang: %d\n", viTriCuaGiaTriX(a, n, x));
-//                        }
+                    if (dong != 0 || cot != 0)
+                    {
+                        System.out.format(" 6. Đếm số lượng số nguyên tố trên một dòng của ma trận.\n");
+                        xuatMaTran();
+                        demSoLuongSNTTungDong();
 
-//                    }
-//                    else
-//                    {
-//                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
-//                        System.out.format("Hay nhan Enter de tiep tuc.\n");
-//                    }
+                    }
+                    else
+                    {
+                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
+                        System.out.format("Hay nhan Enter de tiep tuc.\n");
+                    }
             }
             case 7 -> {
-//                    if (dong != 0 || cot != 0)
-//                    {
-//                        System.out.format(" 7. Tim phan tu lon nhat/ nho nhat trong mang.\n");
-//                        xuatMang(a, n);
-//                        System.out.format("\nPhan tu nho nhat trong mang: %d", elementMin(a, n));
-//                        System.out.format("\nPhan tu lon nhat trong mang: %d\n", elementMax(a, n));
-//                        
-//                    }
-//                    else
-//                    {
-//                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
-//                        System.out.format("Hay nhan Enter de tiep tuc.\n");
-//                    }
+                    if (dong != 0 || cot != 0)
+                    {
+                        System.out.format(" 7. Kiểm tra ma trận có tồn tại số âm hay không.\n");
+                        xuatMaTran();
+                        if(checkTonTaiSoAm() == true)
+                        {
+                            System.out.println("Ma trận có số âm.");
+                        }
+                        else
+                        {
+                            System.out.println("Ma trận không chứa số âm");
+                        }
+                        
+                    }
+                    else
+                    {
+                        System.out.format("Ban chua tao ma tran. Vui long tao ma tran truoc khi thuc hien yeu cau.\n");
+                        System.out.format("Hay nhan Enter de tiep tuc.\n");
+                    }
             }
             case 8 -> {
                 if (dong != 0 || cot != 0)
                 {
+                    System.out.format(" 8. Kiểm tra các phần tử trên dòng k cho trước có tăng dần từ trái qua phải hay không.\n");
+                    xuatMaTran();
+                    int k;
+                    System.out.println("Nhập dòng k: ");
+                    k = scanner.nextInt();
+                    if(checkTangDanTrenK(k) == true)
+                    {
+                        System.out.println("Các phần tử trên "+ k + " tăng dần từ trái qua phải");
+                    }
+                    else
+                    {
+                        System.out.println("Các phần tử trên "+ k + " không tăng dần từ trái qua phải");
 
+                    }
                 }
                 else
                 {
@@ -155,6 +169,10 @@ public class MangHaiChieu
             case 9 -> {
                 if (dong != 0 || cot != 0)
                 {
+                    System.out.format(" 9. Liệt kê dòng có chứa số nguyên tố.\n");
+                    xuatMaTran();
+                    System.out.println("Các dòng chứa số nguyên tố:");
+                    lietKeDongChuaSNT();
 
                 }
                 else
@@ -166,7 +184,10 @@ public class MangHaiChieu
             case 10 -> {
                 if (dong != 0 || cot != 0)
                 {
-
+                    System.out.format("10. Liệt kê dòng chứa toàn giá trị chẵn.\n");
+                    xuatMaTran();
+                    System.out.println("Các dòng chứa toàn giá trị chẵn:");
+                    lietKeDongChuaToanGiaTriChan();
                 }
                 else
                 {
@@ -183,7 +204,7 @@ public class MangHaiChieu
         } while (c != 0);
     }
 
-    private void MENU()
+    public void MENU()
     {
         System.out.format("\n================================ = MENU = =================================\n");
         System.out.format("\t\tHay chon chuong trinh ma ban can thuc hien:\n");
@@ -281,5 +302,180 @@ public class MangHaiChieu
                     System.out.format("\n");
             }
     }
-  
+//   Tinh tong cac gia tri le
+    public int tinhTongCacGiaTriLe()
+    {
+        int total = 0;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(a[i][j] % 2 != 0)
+                {
+                    total += a[i][j];
+                }
+            }
+        }
+        return total;
+    }
+//    Tính tổng các giá trị dương trên dòng k cho trước
+    // 2 3 -4 -5
+    // 9 6 -3 8
+    // 6 8 1 0
+    // k = 1 => total = 23
+    public int tinhTongCacGiaTriDuongTrenK(int k)
+    {
+        int total = 0;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(i == k)
+                {
+                    if(a[k][j] > 0)
+                    {
+                        total += a[k][j];
+                    } 
+                }
+               
+            }
+        }
+        return total;
+    }
+ //Đếm số lượng số dương
+    public int demSoDuong()
+    {
+        int count = 0;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(a[i][j] > 0)
+                {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+    //Đếm số lượng số nguyên tố trên một dòng của ma trận.
+    public boolean checkSNT(int x)
+    {
+        if(x < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+        return true;
+    }
+    
+    public void demSoLuongSNTTungDong()
+    {
+        int count = 0;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(checkSNT(a[i][j]) == true)
+                {
+                    count ++;
+                }
+            }
+            System.out.print("\nDòng "+i+ " có "+ count+" số nguyên tố\n");
+
+        }       
+    }
+
+    // Kiểm tra ma trận có tồn tại số âm hay không
+    public boolean checkTonTaiSoAm()
+    {
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(a[i][j] < 0)
+                {
+                   return true; 
+                }
+            }
+        }
+        return false;
+    }
+    
+   //  Kiểm tra các phần tử trên dòng k cho trước có tăng dần từ trái qua phải hay không
+    
+     public boolean  checkTangDanTrenK(int k)
+    {
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(i == k)
+                {
+                    if(a[k][j] > a[k][j + 1])
+                    {
+                       return false;
+                    }
+                }
+               
+            }
+        }
+        return true;
+    }
+    // Liệt kê dòng có chứa số nguyên tố
+    public void lietKeDongChuaSNT()
+    {
+        int flag = 0;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(checkSNT(a[i][j]) == true)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 1)
+            {
+                System.out.print("\nDòng "+i+ " chứa số nguyên tố\n");
+            }
+        }
+
+    }
+    // Liệt kê dòng chứa toàn giá trị chẵn
+    private boolean checkSoChan(int num)
+    {
+        if(num % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void lietKeDongChuaToanGiaTriChan()
+    {
+        int flag = 1;
+        for (int i = 0; i < dong; i++)
+        {
+            for (int j = 0; j < cot; j++)
+            {
+                if(checkSoChan(a[i][j]) == false)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1)
+            {
+                System.out.print("\nDòng "+i+ " chứa toan gia tri chan\n");
+            } 
+        }       
+    }
 }
